96ea31e9243815515da20b83fd4a543f36116c49
Michel Neumann <neumann.michel93@gmail.com>
2018-06-10 12:01:52 +0200

    add calculations

--- src/app/exposure/exposure.component.ts
+++ src/app/exposure/exposure.component.ts
@@ -7,14 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExposureComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit() {
-    console.warn(ExposureComponent.calculateExposureValue(5.6, 100, 0.1));
+    console.warn(ExposureComponent.calculateTimeValue(5.6, 100, 8.3
+    ));
   }
 
   private static calculateExposureValue(fstop: number, iso: number, time: number): number {
     return Math.log2(100 * Math.pow(fstop, 2) / (iso * time))
+  }
+
+  private static calculateFStop(ev: number, iso: number, time: number): number {
+    return Math.sqrt((Math.pow(2, ev) * iso * time) / 100);
+  }
+
+  private static calculateISOValue(fstop: number, ev: number, time: number): number {
+    return 100 * Math.pow(fstop, 2) * time / Math.pow(2, ev);
+  }
+
+  private static calculateTimeValue(fstop: number, iso: number, ev: number): number {
+    return 100 * Math.pow(fstop, 2) / (iso * Math.pow(2, ev));
   }
 
 }
