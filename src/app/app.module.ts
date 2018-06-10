e44e41b276a95b91d7d3c507a3db70c69f453562
Tobias Scheck <tobias@scheck-media.de>
2018-06-10 12:54:17 +0200

    i dont know what im doing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { MatCardModule } from '@angular/material';
+import { MatCardModule, MatDividerModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MatSelectModule, } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { DofComponent } from './dof/dof.component';
@@ -16,7 +17,9 @@ import { ExposureComponent } from './exposure/exposure.component';
     ExposureComponent
   ],
   imports: [
+    MatDividerModule,
     BrowserModule,
+    FormsModule,
     MatCardModule,
     MatSelectModule,
     BrowserAnimationsModule
--- src/app/dof/dof.component.ts
+++ src/app/dof/dof.component.ts
@@ -10,22 +10,25 @@ import json from '../../assets/camera-sensor-data.json';
 export class DofComponent implements OnInit {
   private data: any = <any> json;
   public vendors: Set<String> = new Set<String>();
+  public selectedModels: Array<any> = new Array<any>();
   public models: {} = {};
+  public dataModel = {vendor: ''};
 
   constructor() {
     for(const camera of this.data) {
       this.vendors.add(camera.CameraMaker);
 
-      if (!(this.models['camera.CameraMaker'] instanceof Array)){
-        this.models['camera.CameraMaker'] = [];
+      if (!(this.models[camera.CameraMaker] instanceof Array)){
+        this.models[camera.CameraMaker] = [];
       }
 
-      this.models['camera.CameraMaker'].push(camera.CameraModel);
+      this.models[camera.CameraMaker].push(camera.CameraModel);
     }
   }
 
   public selectVendor(evt:any){
-    console.log("changed: ", evt);
+    this.selectedModels = this.models[this.dataModel.vendor];
+    console.log(this.selectedModels);
   }
 
   ngOnInit() {
