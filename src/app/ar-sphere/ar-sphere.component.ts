48cb0be970ac3d2ebae40f72a768ebafbbb33eb1
Tobias Scheck <tobias@scheck-media.de>
2018-06-17 17:57:09 +0200

    changed needle position based on compas position

--- src/app/ar-sphere/ar-sphere.component.ts
+++ src/app/ar-sphere/ar-sphere.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, OnDestroy, ChangeDetectorRef, HostListener} from '@angular/core';
 
-
 @Component({
   selector: 'app-ar-sphere',
   templateUrl: './ar-sphere.component.html',
@@ -16,28 +15,61 @@ export class ArSphereComponent implements OnInit , OnDestroy{
   private scene; 
   private camera: BABYLON.DeviceOrientationCamera;
   private needle;
+  private deviceId; 
+  private alpha = 0.0;
 
-  @HostListener('window:deviceorientation', ["$event"]) handleOrientation(e) {    
-    //console.log(e);  
+  @HostListener('window:deviceorientationabsolute', ["$event"]) 
+  @HostListener('window:deviceorientation', ["$event"]) 
+  handleOrientation(e) {    
+    
     // values explained https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Orientation_and_motion_data_explained
     this.orientationSupported = true;
+    //console.log(e);
+    if(e.absolute) {
+      console.log(e);  
+      this.alpha = e.alpha;
+      this.rotateNeedle(e.alpha);
+    }
   }
 
   constructor(private cdRef: ChangeDetectorRef) {
     this.isMobile = navigator.userAgent.indexOf("Mobile") !== -1;
   }
 
-  ngOnInit() {
-    console.log(navigator.geolocation);
+  ngOnInit() {    
     if(navigator.geolocation){
       navigator.geolocation.getCurrentPosition(position => {        
-        console.log(position.coords); 
+        //console.log(position.coords); 
       }, err => {
         console.log("error: ", err);
       });
     }
 
+    
+
+    let constraint = {
+      audio: false,
+      video: {
+        facingMode: 'environment',
+        width: { min: 720 },
+        height: { min: 1280 }
+      }
+    };  
+
+    this.videoObject = document.createElement('video');
+    navigator.mediaDevices.getUserMedia(constraint)
+    .then((stream) => {
+      this.videoObject.srcObject = stream;
+      this.videoObject.onloadedmetadata = () => {
+        this.videoObject.play();
+        console.log("play", this.videoObject.readyState);
+      }    
+    })
+    .catch(function(err) {
+      console.log(err.name + ": " + err.message);
+    });
     this.initEngine();
+    
 
     this.needle = document.getElementById('needle');
   }
@@ -82,42 +114,27 @@ export class ArSphereComponent implements OnInit , OnDestroy{
     // Video plane
     var videoPlane = BABYLON.Mesh.CreatePlane("Screen", 1, scene);
     videoPlane.position.y = 0;
-    videoPlane.position.z = 100;
-    videoPlane.rotation.z = Math.PI;
-    videoPlane.rotation.y = Math.PI;
+    videoPlane.position.z = 100;    
     videoPlane.parent = this.camera
 
     // Video material
     var videoMat = new BABYLON.StandardMaterial("textVid", scene);    
     videoMat.emissiveColor = new BABYLON.Color3(1,1,1);
     videoMat.backFaceCulling = false;
-
-    let self = this;
-    BABYLON.VideoTexture.CreateFromWebCam(scene, function (videoTexture) {
-      self.videoObject = videoTexture;
-      videoMat.diffuseTexture = self.videoObject;
-    }, {facingMode: 'environment', minWidth: 128, minHeight: 128, maxWidth: 1280, maxHeight: 720 });     
+      
+    videoMat.diffuseTexture = new BABYLON.VideoTexture("livestream", this.videoObject, scene );     
+    videoPlane.material = videoMat;
     
-
-    
-    scene.onBeforeRenderObservable.add(function () {      
-      if (self.videoObject !== undefined && self.hasVideo == false) {          
-          if (self.videoObject.video.readyState == 4) {
-            //Applying materials
-            videoPlane.material = videoMat;
-            self.hasVideo = true;
-          }
-      }
-
-      self.rotateNeedle(new BABYLON.Angle(self.scene.activeCamera.rotationQuaternion.toEulerAngles().y).degrees());
-    });
+    // scene.onBeforeRenderObservable.add(() => {      
+    //   this.rotateNeedle(new BABYLON.Angle(this.scene.activeCamera.rotationQuaternion.toEulerAngles().y).degrees());
+    // });
 
     return scene;
   }
 
   private rotateNeedle(deg) {
     deg -= 45; // 45deg = reset needle to north
-    let mat = this.needle.getCTM();    
+    //let mat = this.needle.getCTM();    
     //let matDeg = ((180 / Math.PI) * Math.atan2(mat.b, mat.a));
     this.needle.setAttribute("transform", "rotate(" + deg + " 17 16)");
   }
