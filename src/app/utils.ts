d2ca6a5a34ff6bc067d8af9bf476218f1f050d00
Tobias Scheck <tobias@scheck-media.de>
2018-06-20 12:44:41 +0200

    conversion from latitude/longitude to cartesian coordiantes

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,7 +1,19 @@
 import * as BABYLON from 'babylonjs';
 
 export class Utils {
-    public static colorToCss(color: BABYLON.Color3) {    
+    public static colorToCss(color: BABYLON.Color3) : String {    
         return 'rgb( ' + color.r * 255.0 + ', ' + color.g * 255.0 + ', ' + color.b * 255.0 +')';
+    }
+
+    public static latLonToXYZ(lat, lon) : BABYLON.Vector3 {        
+        const r = 6378137; // meter                    
+        lat = lat * Math.PI / 180.0;
+        lon = lon * Math.PI / 180.0;
+        
+        let x = r * Math.cos(lat) * Math.cos(lon);
+        let y = r * Math.cos(lat) * Math.sin(lon);
+        let z = r * Math.sin(lat);
+
+        return new BABYLON.Vector3(x,y,z);
     }
 }
