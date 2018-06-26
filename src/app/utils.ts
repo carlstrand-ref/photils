b0012effd07fde1e53a79a4ce5aaae514df1e54b
Tobias Scheck <tobias@scheck-media.de>
2018-06-26 15:13:22 +0200

    added hue to color3 conversion

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -16,4 +16,33 @@ export class Utils {
 
         return new BABYLON.Vector3(x,y,z);
     }
+
+    public static hueToColor3(h, s, v) : BABYLON.Color3 {
+        if (s === 0 ) {
+            return new BABYLON.Color3(v, v, v);
+        }
+
+        let hi = Math.floor(h / 60);
+        let f = h / 60 - hi;
+
+        let p = v * (1 - s);
+        let q = v * (1 - s * f);
+        let t = v * (1 - s * (1 - f));
+
+        switch(hi) {
+            case 0:
+            case 6:
+                return new BABYLON.Color3(v, t, p);
+            case 1:
+                return new BABYLON.Color3(q, v, p);
+            case 2:
+                return new BABYLON.Color3(p, v, t);
+            case 3:
+                return new BABYLON.Color3(p, q, v);
+            case 4:
+                return new BABYLON.Color3(t, p, v);
+            case 5:
+                return new BABYLON.Color3(v, q, p);
+        }
+    }
 }
