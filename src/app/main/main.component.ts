48c55ed24451aeb289b8e4c25691147c5ec56e29
Tobias Scheck <tobias@scheck-media.de>
2018-06-29 13:25:19 +0200

    added dev mode warning to main screen

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { isDevMode } from '@angular/core';
 
 @Component({
   selector: 'app-main',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-
+  private isDevMode: boolean = isDevMode()
   constructor() { }
 
   ngOnInit() {
