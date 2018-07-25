000fe95c12c91cbf170cb7c912c0928782c7c3b7
Tobias Scheck <tobias@scheck-media.de>
2018-07-25 14:15:35 +0200

    version info

---
+++ src/environments/version.ts
@@ -0,0 +1,14 @@
+// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* tslint:disable */
+export const VERSION = {
+    "dirty": true,
+    "raw": "b6af28d-dirty",
+    "hash": "b6af28d",
+    "distance": null,
+    "tag": null,
+    "semver": null,
+    "suffix": "b6af28d-dirty",
+    "semverString": null,
+    "version": "0.1.0-beta"
+};
+/* tslint:enable */
