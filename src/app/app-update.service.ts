dd1677d927ce4ae9cacf34e3608ddedb77c6ffe6
Tobias Scheck <tobias@scheck-media.de>
2018-07-26 12:01:47 +0200

    added update service to display update message if available

---
+++ src/app/app-update.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { MatSnackBar } from '@angular/material';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppUpdateService {
+
+  constructor(private swUpdate: SwUpdate, private snackbar: MatSnackBar) {
+    this.swUpdate.available.subscribe(evt => {
+      const snack = this.snackbar.open('Update Available', 'Reload', { panelClass: 'success'});
+
+      snack
+        .onAction()
+        .subscribe(() => {
+          window.location.reload();
+        });
+    });
+  }
+}
